c29227373d9e329368b4ddf1090bd2c5f8c508d3
Sundeep <sundeep.kaler@gmail.com>
2024-08-01 17:29:05 -0400

    complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,24 @@
-// Your tests here
+import { isPalindrone } from "../utils";
+
+describe("isPalindrome", () => {
+    it("tests if word is a palindrome", () => {
+        const word1 = "racecar";
+        const palinBool = isPalindrone(word1);
+        expect(palinBool).toBe(true)
+        
+    });
+    it("tests if word is not a palindrome", () => {
+        const word = "car";
+        const palinBool = isPalindrone(word);
+        expect(palinBool).toBe(false);
+    });
+    it("tests if word is still a palindrome with capitalized letters", () => {
+        const word = "raCecAr";
+        const palinBool = isPalindrone(word);
+        expect(palinBool).toBe(false);
+    });
+    it("tests if an error is thrown when string is empty", () => {
+        const word = "";
+        expect(() => isPalindrome(word)).toThrow();
+    });
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,5 @@
-// Your code here
+export function isPalindrone(word) {
+    if(!/^[A-Za-z]+$/.test(word))
+        throw new Error("Invalid input")
+    return word == word.toLowerCase().split('').reverse().join('');
+}
